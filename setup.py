18463a1cc149f542e263b8ea9d4a0485f8e233e0
Dark Night Hacker <102655446+DNhacker@users.noreply.github.com>
2025-11-07 20:10:31 +0530

    Add setup.py for yt-dl package configuration

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().splitlines()
+
+setup(
+    name="yt-dl",
+    version="0.1.0",
+    author="DNhacker",
+    author_email="Darknighthacker0@gmail.com",
+    description="A Python library to download YouTube videos in HD MP4 and MP3 formats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "yt-dl=yt_dl.downloader:main",
+        ],
+    },
+    keywords="youtube download video mp4 mp3 hd",
+    url="https://github.com/DNhacker/yt_dl",
+)
