0e9ebfad8e5c4f24b1c424318ff8251a49aaaeaf
Dark Night Hacker <102655446+DNhacker@users.noreply.github.com>
2025-11-07 20:13:24 +0530

    Initialize YouTube HD Downloader library

---
+++ src/yt_dl/init.py
@@ -0,0 +1,10 @@
+"""
+YouTube HD Downloader
+A library to download YouTube videos in HD MP4 and MP3 formats.
+"""
+
+from .downloader import YouTubeDownloader
+
+__version__ = "0.1.0"
+__author__ = "DNhacker"
+__all__ = ["YouTubeDownloader"]
